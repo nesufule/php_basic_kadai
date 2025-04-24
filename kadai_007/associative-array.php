4aa144739c5e8770412e12d9d56145babb3e007c
nesufule <nesufule@gmail.com>
2025-04-24 23:55:51 +0900

    kadai_007をアップ

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+
+<head>
+
+    <meta charset="UTF-8">
+    <title>kadai_007</title>
+
+</head>
+<body>
+    <p>
+        <?php
+
+        $array = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+        echo '<br>';
+
+        print_r($array);
+
+        ?>
+    </p>
+<body>
+
+</html>
+
